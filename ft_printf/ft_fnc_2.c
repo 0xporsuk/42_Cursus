0229a488f74392273b36bd0c78646c8f1c1d1240
Muhammet Dönmezer <mdonmeze@k1m25s09.42istanbul.com.tr>
2024-12-09 20:26:15 +0300

    update

--- ft_printf/ft_fnc_2.c
+++ ft_printf/ft_fnc_2.c
@@ -6,7 +6,7 @@
 /*   By: mdonmeze <mdonmeze@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/16 02:59:46 by mdonmeze          #+#    #+#             */
-/*   Updated: 2024/11/18 19:39:57 by mdonmeze         ###   ########.fr       */
+/*   Updated: 2024/12/09 20:17:22 by mdonmeze         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,8 +21,8 @@ int	ft_unsint(unsigned int a)
 		i += ft_putnbr(a);
 	else if (a > 57)
 	{
-		i += ft_uns(a / 10);
-		i += ft_uns(a % 10);
+		i += ft_unsint(a / 10);
+		i += ft_unsint(a % 10);
 	}
 	return (i);
 }
--- ft_printf/ft_printf.c
+++ ft_printf/ft_printf.c
@@ -6,7 +6,67 @@
 /*   By: mdonmeze <mdonmeze@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/15 23:01:53 by mdonmeze          #+#    #+#             */
-/*   Updated: 2024/11/18 19:50:58 by mdonmeze         ###   ########.fr       */
+/*   Updated: 2024/12/09 20:17:28 by mdonmeze         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "ft_printf.h"
+#include "stdarg.h"
+
+int	format(char c, va_list arg)
+{
+	int	i;
+
+	i = 0;
+	if (c == 'c')
+		i += ft_putchar(va_arg(arg, int));
+	else if (c == 's')
+		i += ft_putstr(va_arg(arg, char *));
+	else if (c == 'i')
+		i += ft_putnbr(va_arg(arg, int));
+	else if (c == 'd')
+		i += ft_putnbr(va_arg(arg, int));
+	else if (c == 'u')
+		i += ft_unsint(va_arg(arg, unsigned int));
+	else if (c == 'x')
+		i += ft_hex(va_arg(arg, unsigned int), 0);
+	else if (c == 'X')
+		i += ft_hex(va_arg(arg, unsigned int), 1);
+	else if (c == 'p')
+	{
+		i += ft_putstr("0x");
+		i += ft_ptr(va_arg(arg, unsigned long), 0);
+	}
+	else
+		i += ft_putchar('%');
+	return (i);
+}
+
+int	ft_printf(const char *str, ...)
+{
+	int		i;
+	int		count;
+	va_list	arg;
+
+	va_start(arg, str);
+	i = 0;
+	count = 0;
+	while (str[i])
+	{
+		if (str[i] == '%')
+		{
+			i++;
+			while (str[i] == ' ' && str[i])
+				i++;
+			count += format(str[i], arg);
+			i++;
+		}
+		else
+		{
+			count += ft_putchar(str[i]);
+			i++;
+		}
+	}
+	va_end(arg);
+	return (count);
+}
